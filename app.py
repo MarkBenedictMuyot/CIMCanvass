6bcfe776bdee35dad9e90f204ba6131d9018710c
unknown <markmuyot@hotmail.com>
2022-12-23 02:20:52 -0500

    linked all pages and added about and contact html

--- app.py
+++ app.py
@@ -6,13 +6,66 @@ app = Flask(__name__)
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html',
-                           product=url_for('product')
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
                            )
 
 
 @app.route('/product')
 def product():  # put application's code here
     return render_template('product.html',
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
+                           )
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html',
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
+                           )
+
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return render_template('contact.html',
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
+                           )
+
+
+@app.route('/account')
+def account():  # put application's code here
+    return render_template('account.html',
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
+                           )
+
+
+@app.route('/cart')
+def cart():  # put application's code here
+    return render_template('cart.html',
+                           product=url_for('product'),
+                           account=url_for('account'),
+                           about=url_for('about'),
+                           contact=url_for('contact'),
+                           cart=url_for('cart')
                            )
 
 
